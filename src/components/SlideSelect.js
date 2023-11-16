ac8d415adfc37a8679f28896c8026a17ab634ca3
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-16 19:58:39 +1300

    2nd commit

--- src/components/SlideSelect.js
+++ src/components/SlideSelect.js
@@ -5,11 +5,12 @@ const SlideSelect = () => {
   return (
     <div>
       <SliderComponent
-        title="1"
+        title={1}
         defaultValue={25}
         min={0}
         max={1000}
         step={100}
+        onChange={(e, value) => console.log(value)}
       />
       {/* <SliderComponent title="2" />
       <SliderComponent title="3" /> */}
--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
 
-const SliderComponent = ({ title, min, max, defaultValue, step }) => {
+const SliderComponent = ({
+  title,
+  min,
+  max,
+  defaultValue,
+  step,
+  onChange,
+  value,
+}) => {
   return (
     <div>
       {title && <h3>{title}</h3>}
@@ -13,7 +21,17 @@ const SliderComponent = ({ title, min, max, defaultValue, step }) => {
         step={step}
         aria-label="Default"
         valueLabelDisplay="auto"
+        onChange={onChange}
+        value={value}
       />
+      <div className="sliderRange">
+        <div className="sliderRangeL">
+          <h3>$1000</h3>
+        </div>
+        <div className="sliderRangeR">
+          <h3>$10000</h3>
+        </div>
+      </div>
     </div>
   );
 };
